1a02383d43b8f97871f446cb2c189ea8467c6289
PaNkaj <147901370+PANKAjBamniya@users.noreply.github.com>
2025-03-13 18:48:10 +0530

    update-

--- frontend/src/features/todos/todoService.js
+++ frontend/src/features/todos/todoService.js
@@ -1,21 +1,26 @@
 import axios from "axios";
 
+const API_URI = process.env.REACT_APP_API_URL; // Ensure this is set in .env
+
 export const fetchTodo = async () => {
-  const response = await axios.get(`/api/todo`);
+  const response = await axios.get(`${API_URI}/api/todo`); // Use API_URI
   return response.data;
 };
 
 export const createTodo = async (formData) => {
-  const response = await axios.post(`/api/todo`, formData);
+  const response = await axios.post(`${API_URI}/api/todo`, formData); // Use API_URI
   return response.data;
 };
 
 export const deleteTodo = async (id) => {
-  const response = await axios.delete("/api/todo/" + id);
+  const response = await axios.delete(`${API_URI}/api/todo/${id}`); // Use API_URI
   return response.data;
 };
 
-export const update = async (fromData) => {
-  const response = await axios.put("/api/todo/" + fromData);
+export const updateTodo = async (formData) => {
+  const response = await axios.put(
+    `${API_URI}/api/todo/${formData.id}`,
+    formData
+  ); // Use API_URI
   return response.data;
 };
